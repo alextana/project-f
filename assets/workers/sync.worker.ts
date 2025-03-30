3114d8013994366eade9e5853b0645c1c9eff4fb
Alex Tana <alex.tana1992@gmail.com>
2025-03-30 03:59:23 +0100

    add web worker for sync (initial)

--- assets/workers/sync.worker.ts
+++ assets/workers/sync.worker.ts
@@ -6,8 +6,24 @@ import { db } from '~/lib/dexie'
  * the dexie db with the server.
  */
 
-self.addEventListener('message', (event) => {
-  console.log(db)
-  console.log('event', event)
-  console.log('receive messsage')
+// self.addEventListener('message', (event) => {
+//   console.log(db)
+//   console.log('event', event)
+//   console.log('receive messsage')
+// })
+
+self.addEventListener('message', async (event: MessageEvent) => {
+  if (event.data === 'sync') {
+    console.log('SYNC ME PLEASE!!!')
+
+    try {
+      self.postMessage({ status: 'success', message: 'sync happened!' })
+    } catch (error) {
+      self.postMessage({
+        status: 'error',
+        message: 'something went wrong..',
+        error: error,
+      })
+    }
+  }
 })
--- composables/useSyncWorker.ts
+++ composables/useSyncWorker.ts
@@ -1,7 +1,27 @@
 import Worker from '@/assets/workers/sync.worker.ts?worker'
 
 // the worker should probably be a singleton
-const worker: Worker | null = new Worker()
+let worker: Worker | null = null
+
+/**
+ * Only create a worker in case
+ * we don't have one
+ */
+if (!worker) {
+  worker = new Worker()
+
+  worker.addEventListener('message', (event: MessageEvent) => {
+    const { data } = event
+
+    if (data.status === 'success') {
+      console.log(data.message)
+    }
+
+    if (data.status === 'error') {
+      console.log(data.message, data.error)
+    }
+  })
+}
 
 export const useSyncWorker = () => {
   const postToWorker = () => {
