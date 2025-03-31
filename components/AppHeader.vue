2ed176c75e700a3f2bd12e2897c9d06bf9b0d4ed
Alex Tana <alex.tana1992@gmail.com>
2025-03-31 03:14:15 +0100

    changes to login flow to setup intercepting

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,9 @@
     class="w-screen py-3 px-4 border-b bg-slate-800/20 border-gray-700/40 shadow-md"
   >
     <div class="flex justify-between items-center">
-      <NuxtLink to="/">Logo</NuxtLink>
+      <NuxtLink to="/"
+        ><span class="font-extrabold italic">Local first</span> notes</NuxtLink
+      >
       <div v-if="!session.isPending" class="user-info flex items-center gap-2">
         <UAvatar
           v-if="session.data?.user?.image"
@@ -26,14 +28,14 @@
 </template>
 
 <script setup lang="ts">
-import { signIn, signOut, useSession } from '~/lib/auth-client'
+import { signOut, useSession } from '~/lib/auth-client'
 const session = useSession()
+const { socialSignIn } = useLogin()
 
-const login = async () =>
-  await signIn.social({
-    provider: 'google',
-    callbackURL: '/',
-    errorCallbackURL: '/error',
-    newUserCallbackURL: '/',
+const login = () =>
+  socialSignIn('google', {
+    callbackURL: '/notes?loggedIn=true',
+    errorCallbackURL: '/login/error',
+    newUserCallbackURL: '/first-login',
   })
 </script>
