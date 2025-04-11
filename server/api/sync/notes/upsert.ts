9d692ec9e9796a12efacfe4a0f6dca41047ef874
Alex Tana <alex.tana1992@gmail.com>
2025-04-12 00:17:35 +0100

    add validation for note content

--- server/api/sync/notes/upsert.ts
+++ server/api/sync/notes/upsert.ts
@@ -1,6 +1,8 @@
 import { auth } from '~/lib/auth'
 import { db } from '~/server/utils/db'
 import { note } from '~/server/db/schema'
+import { z } from 'zod'
+import superjson from 'superjson'
 
 export default defineEventHandler(async (event) => {
   const session = await auth.api.getSession({
@@ -14,6 +16,26 @@ export default defineEventHandler(async (event) => {
       statusCode: 422,
       statusMessage: 'Cannot sync without the action type',
     })
+  }
+
+  // validate the content to make sure
+  // it's a valid tiptap content shape
+  const NoteContent = z.object({
+    type: z.string(),
+    content: z.array(z.any()),
+  })
+
+  try {
+    NoteContent.parse(superjson.parse(body.note.content))
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.error('Validation errors:', error.errors)
+
+      throw createError({
+        statusCode: 422,
+        statusMessage: 'The note content is not a valid tiptap content shape',
+      })
+    }
   }
 
   if (body.note.userId !== session?.user.id) {
